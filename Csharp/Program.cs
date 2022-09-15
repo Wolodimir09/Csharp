2f729f90c9babf4c28d77f28754de09ebed57eb1
Владимир Лысенко <lysiza09@gmail.com>
2022-09-15 16:29:33 +0300

    Update class Program

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -10,6 +10,22 @@ namespace Csharp
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            
+            ////////////////////////////////////////////////////////////////////
+            // Завдання 1 
+
+            Array arr = new Array(12, 3, 2, 5, 12, 35, 67, 5, 12,87);
+            Console.WriteLine("------------- Завдання 1 ---------------");
+            arr.Show();
+            Console.WriteLine("\n---------------------------------------");
+            arr.Show("Додаткова інформація до масиву цілих чисел:");
+            Console.WriteLine("\n---------------------------------------");
+
+            // Завдання 2
+
+
         }
     }
 }
