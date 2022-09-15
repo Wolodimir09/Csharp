f6d1d776078a64cea498d71d6646196d75ea0990
Владимир Лысенко <lysiza09@gmail.com>
2022-09-15 22:24:52 +0300

    update info class Program

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -17,7 +17,7 @@ namespace Csharp
             // Завдання 1 
 
             Array arr = new Array(12, 3, 2, 5, 12, 35, 67, 5, 12,86);
-            Console.WriteLine("------------- Завдання 1 ---------------");
+            Console.WriteLine("\n------------- Завдання 1 ---------------");
             arr.Show();
             Console.WriteLine("\n---------------------------------------");
             arr.Show("Додаткова інформація до масиву цілих чисел:");
@@ -25,13 +25,26 @@ namespace Csharp
 
             // Завдання 2
 
-            Console.WriteLine("------------- Завдання 2 ---------------");            
+            Console.WriteLine("\n------------- Завдання 2 ---------------");            
             Console.WriteLine("Максимальне значення: " + arr.Max());
             Console.WriteLine("Мінімальне значення: " + arr.Min());
             Console.WriteLine("Середнє значення: " + arr.Avg());
             Console.WriteLine("Наявність елементів 5 і 9: " + arr.Search(5) + " || " +
                                                                arr.Search(9));
-            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("////////////////////////////////////////");
+           
+            // Завдання 3            
+            
+            Console.WriteLine("\n------------- Завдання 3 ---------------");
+            arr.SortAsc();
+            arr.Show("Масив після сортування за зростанням:");
+            Console.WriteLine("\n---------------------------------------");
+            arr.SortDesc();
+            arr.Show("Масив після сортування за спаданням:");
+            Console.WriteLine("\n---------------------------------------");
+            arr.SortByParam(true);
+            arr.Show("Масив після сортування за зростанням:");
+            Console.WriteLine("\n////////////////////////////////////////");
         }
     }
 }
