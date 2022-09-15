2359bd95e48d50cfc77eeaa3befb219ae689b430
Владимир Лысенко <lysiza09@gmail.com>
2022-09-15 16:25:13 +0300

    Update class Array

--- Csharp/Array1.cs
+++ Csharp/Array1.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Csharp
 {
-    internal class Array1
+    internal class Array
     {
+        private int[] arr;
+        public Array(params int[] arr)
+        {
+            this.arr = arr;
+        }
+        public void Show()
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+        }
+
+        public void Show(string info)
+        {
+            Console.WriteLine(info);
+            Show();
+        }
     }
 }
