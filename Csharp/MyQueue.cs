a68709e1875207c5c5e8326b90d7a4730606571a
Владимир Лысенко <lysiza09@gmail.com>
2022-09-25 17:43:36 +0300

    Update class MyQueue<T>

--- Csharp/MyQueue.cs
+++ Csharp/MyQueue.cs
@@ -8,5 +8,45 @@ namespace Csharp
 {
     internal class MyQueue<T>
     {
+        List<T> queue;
+        public int count { get; private set; }
+
+        public MyQueue()
+        {
+            queue = new List<T>();
+        }
+        public T Dequeue()
+        {
+            if (count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            T dequeue = queue[0];
+            queue.Remove(queue[0]);
+            count--;
+            return dequeue;
+        }
+        public T Peek()
+        {
+            if (count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            return queue[0];
+        }
+        public void Enqueue(T value)
+        {
+            queue.Add(value);
+            count++;
+        }
+        public override string ToString()
+        {
+            string stQueue = " ";
+            foreach (var item in queue)
+            {
+                stQueue += item.ToString() + " ";
+            }
+            return stQueue;
+        }
     }
 }
