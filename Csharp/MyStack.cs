c28f410fecdf9e07fa9083518a720ece02a2339a
Владимир Лысенко <lysiza09@gmail.com>
2022-09-25 17:19:56 +0300

    Update class MyStack

--- Csharp/MyStack.cs
+++ Csharp/MyStack.cs
@@ -8,5 +8,46 @@ namespace Csharp
 {
     internal class MyStack<T>
     {
+        List<T> stack;
+        public int count { get; private set; }
+
+        public MyStack()
+        {
+            stack = new List<T>();
+        }
+        public T Pop()
+        {
+            if (count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            T pop = stack[stack.Count - 1];
+            stack.RemoveAt(stack.Count - 1);
+            count--;
+            return pop;
+        }
+        public T Peek()
+        {
+            if (count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            T pop = stack[stack.Count - 1];
+            return pop;
+        }
+        public void Push(T value)
+        {
+            stack.Add(value);
+            count++;
+        }
+        public override string ToString()
+        {
+            string stStack = " ";
+            for (int i = count - 1; i >= 0; i--)
+            {
+                stStack += stack[i].ToString() + " ";
+            }
+            return stStack;
+        }
     }
 }
