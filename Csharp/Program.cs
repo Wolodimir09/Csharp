5dadbca36249307e74787686c12d3f3b3c50ee8d
Владимир Лысенко <lysiza09@gmail.com>
2022-09-27 16:37:19 +0300

    Update class Program

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -11,7 +11,39 @@ namespace Csharp
         public delegate double MyCalcDel(double a, double b);
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
 
+            try
+            {
+                Console.WriteLine("Введіть 2 числа: ");
+                double value1 = double.Parse(Console.ReadLine());
+                double value2 = double.Parse(Console.ReadLine());
+                Console.WriteLine("----------------------------  \nРезультат розрахунків: ");
+                MyCalcDel calcDel = MyMath.Add;
+                calcDel += MyMath.Mult;
+                calcDel += MyMath.Sub;
+
+
+                foreach (MyCalcDel item in calcDel.GetInvocationList())
+                {
+                    switch (item.Method.Name)
+                    {
+                        case "Add": { Console.Write($"{value1} + {value2} = "); } break;
+                        case "Mult": { Console.Write($"{value1} * {value2} = "); } break;
+                        case "Sub": { Console.Write($"{value1} - {value2} = "); } break;
+                    }
+                    Console.WriteLine(item(value1, value2));
+                }
+            }
+            catch (MemberAccessException member)
+            {
+                Console.WriteLine(member.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
