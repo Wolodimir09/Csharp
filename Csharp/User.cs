a1bda184eb3d921bea7c1bb2bf434b7d6b09d5d2
Владимир Лысенко <lysiza09@gmail.com>
2022-09-27 15:49:13 +0300

    Update class User

--- Csharp/User.cs
+++ Csharp/User.cs
@@ -8,6 +8,20 @@ namespace Csharp
 {
     internal class User
     {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public int UserId { get; private set; }
+        public static int CountUser { get; private set; } = 0;
+        public User()
+        {
+            CountUser++;
+            UserId = CountUser;
+        }
 
+        public override string ToString()
+        {
+            return $"Ім'я користувача: \t{Name}\nДата реєстрації: \t{Date.ToShortDateString()}\n" +
+                    $"Користувацький ID: \t{UserId}";
+        }
     }
 }
