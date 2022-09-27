a33439b417ae4175c22f2d1e92780bf573cc049c
Владимир Лысенко <lysiza09@gmail.com>
2022-09-27 16:28:20 +0300

    Update class  MyMath

--- Csharp/MyMath.cs
+++ Csharp/MyMath.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace Csharp
 {
-    internal class MyMath
+    internal static class MyMath
     {
+        public static double Add(double a, double b) => a + b;
+        public static double Sub(double x, double y) => x - y;
+        public static double Mult(double x, double y) => x * y;
     }
 }
