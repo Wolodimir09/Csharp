22f0161bc320b33d351c4c8a1f1514a2ebf24c55
Владимир Лысенко <lysiza09@gmail.com>
2022-09-29 16:09:37 +0300

    Add class MyExceptionCreditCart

---
+++ Csharp/MyException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp
+{
+    internal class MyExceptionCreditCart:ApplicationException
+    {
+        public DateTime TimeExceptionBancrot { get; private set; }
+        public MyExceptionCreditCart() : base("Помилка банківської карти!!!")
+        {
+            TimeExceptionBancrot = DateTime.Now;
+        }
+    }
+}
