34a1103397fa0b4512de0c40098a8a445ab9ba5e
Владимир Лысенко <lysiza09@gmail.com>
2022-09-29 16:10:12 +0300

    Update class CreditCard

--- Csharp/CreditCard.cs
+++ Csharp/CreditCard.cs
@@ -25,13 +25,40 @@ namespace Csharp
     internal class CreditCard
     {
         public static long CardNumber { get; private set; } = 0;
-        public string[] NameOfTheOwner { get;private set; }
+        public string NameOfTheOwner { get;private set; }
         public DateTime Validity { get; private set; }
         private int _pinCode;
+        private int _pinCodeError = 3;
         public double CreditLimit { get; private set; }
         public double Balance { get; private set; }
 
+        private Random rand = new Random();
+        public CreditCard(string firstName,string lastName, string middlName)
+        {
+            CardNumber++;
+            NameOfTheOwner = middlName + " " + firstName + " " + lastName;
+            Validity = DateTime.Now.AddYears(3);
+            _pinCode = rand.Next(1, 9999);
+            CreditLimit = 10000;
+            Balance = CreditLimit;
+        }
 
+        public void Refill(int pinCode, double money)
+        {
+            if (pinCode==_pinCode)
+            {
+                Balance += money;
+                _pinCodeError = 3;
+            }
+            else
+            {
+                _pinCodeError--;
+                if (_pinCodeError==0)
+                {
+                   new throw MyException("Карту заблоковано! Зверніться до найближчого відділення банку!!!")
+                }
+            }
+        }
 
 
 
