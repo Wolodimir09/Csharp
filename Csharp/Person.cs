76d6e29f914f0cc6de08d87471ccec0bcfaa674d
Владимир Лысенко <lysiza09@gmail.com>
2022-09-29 16:08:48 +0300

    Update class Person

--- Csharp/Person.cs
+++ Csharp/Person.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Csharp
 {
+    public delegate Func<string, string> mesageDel(string mesage);
     internal class Person
-    {
+    {         
+        public event mesageDel mesageDel;
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        private CreditCard card = null;
+
+        public Person(string firstName, string lastName, string middleName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            MiddleName = middleName;
+            card = new CreditCard(firstName,lastName,middleName);
+        }
     }
 }
