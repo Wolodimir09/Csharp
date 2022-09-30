720f718c1b14cebe49eb7b5fbce6dc80a28284b4
Владимир Лысенко <lysiza09@gmail.com>
2022-09-30 17:07:13 +0300

    Update class Program - Problem3, Problem4, Problem5

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -19,15 +19,45 @@ namespace Csharp
         {
             Console.OutputEncoding=Encoding.UTF8;
             Console.InputEncoding=Encoding.UTF8;
-            Console.WriteLine("-------------------- Задача 1 -----------------------"); 
-            Problem1();
-            Console.WriteLine("\n-------------------- Задача 3 -----------------------");
-            Problem3();
+            try
+            {
+                Console.WriteLine("-------------------- Задача 1 -----------------------");
+                Problem1();
+                Console.WriteLine("\n-------------------- Задача 3-5 -----------------------");
+                Console.Write("Введіть значення масиву цілих цисел чкркз пробіл: ");
+                int[] arrayInt = Array.ConvertAll(Console.ReadLine().Split(), Convert.ToInt32);
+                Problem3(arrayInt);
+                Problem4(arrayInt);
+                Problem5(arrayInt);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
         }
 
-        private static void Problem3()
+        private static void Problem5(int[] arrayInt)
         {
-            throw new NotImplementedException();
+            Func<int[], int[]> func = (value) => value.Where(e => e < 0).Distinct().ToArray();
+            Console.Write("Кількість унікальних негативних значень масиву: ");
+            foreach (var item in func(arrayInt))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+        private static void Problem4(int[] arrayInt)
+        {
+            Func<int[], int> func = (value) => value.Where(e => e>0).Count();
+            Console.WriteLine("Кількість позитивних значень масиву: " + func(arrayInt));
+        }
+
+        private static void Problem3(int[] arrayInt)
+        {
+            Func<int[],int> func = (value)=>value.Where(e=>e%7==0).Count();
+            Console.WriteLine("Кількість значень кратних 7-ми: "+func(arrayInt));
         }
 
         private static void Problem1()
@@ -50,16 +80,9 @@ namespace Csharp
                     return dicRainbow[color];
                 }
                 throw new Exception("Значення не знайдено");
-            };
-            try
-            {
+            };            
                 Console.Write("Вкажіть один із кольорів веселки англійською: ");
-                Console.WriteLine("RGB: " + colorsOfRainbow(rainbow, Console.ReadLine()));
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+                Console.WriteLine("RGB: " + colorsOfRainbow(rainbow, Console.ReadLine()));            
         }
     }
 }
