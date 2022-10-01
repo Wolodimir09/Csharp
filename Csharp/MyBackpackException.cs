721abcf9e3618a6bef9eecc3dddd74a0a661de61
Владимир Лысенко <lysiza09@gmail.com>
2022-10-01 19:39:05 +0300

    Add and update MyBackpackException

---
+++ Csharp/MyBackpackException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Csharp
+{
+    [Serializable]
+    internal class MyBackpackException : Exception
+    {
+        public MyBackpackException()
+        {
+        }
+
+        public MyBackpackException(string message) : base(message)
+        {
+            
+        }
+
+        
+    }
+}
