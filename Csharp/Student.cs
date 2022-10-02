afa80cd6d66c0947c2c4401d32587e67c6f2cc75
Владимир Лысенко <lysiza09@gmail.com>
2022-10-02 16:34:26 +0300

    Add and update class Student

---
+++ Csharp/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp
+{
+    internal class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Years { get; set; }
+        public string NameGroup { get; set; }
+
+        public override string ToString()
+        {
+            return $"Імя - {FirstName}\n"+
+                $"Фамілфя - {LastName}\n" +
+                $"Вік - {Years}\n" +
+                $"Група - {NameGroup}\n";
+        }
+
+    }
+}
