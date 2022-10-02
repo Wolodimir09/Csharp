9a4b4ffa37635a7b652a0b2de2e28a5b8195d5d2
Владимир Лысенко <lysiza09@gmail.com>
2022-10-02 21:27:40 +0300

    Update class Recipe

--- Csharp/Recipe.cs
+++ Csharp/Recipe.cs
@@ -6,13 +6,23 @@ using System.Threading.Tasks;
 
 namespace Csharp
 {
-    internal class Recipe
+    public class Recipe
     {
         public string Name { get; set; }
         public string NameKithen { get; set; }
-        public string NameIngredient { get; set; }
+        public string[] NameIngredients { get; set; }
         public DateTime CookingTime { get; set; }
-        public string Descripyion { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return $"{"Назва рецепту:",15} {Name,-15}\n" +
+                   $"{"Назва кухні:",15} {NameKithen,-15}\n" +
+                   $"{"Список інгредієнтів:",15} {NameIngredients.Where(e => e != null).GetEnumerator(),-15}\n" +
+                   $"{"Час приготування:",15} {CookingTime,-15}\n" +
+                   $"{"Покроковий опис:",15} {Description,-200}\n";
+                ;
+        }
 
     }
 }
